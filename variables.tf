dfdafa34ab1291069778d38ced0e27393b1ecef3
Dare Omotayo <dareomotayo32@gmail.com>
2025-08-22 21:47:59 +0100

    remove duplicate variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
 variable "netlify_token" {
+  description = "Personal access token for Netlify API"
   type        = string
-  description = "Netlify Personal Access Token"
   sensitive   = true
 }
 
-variable "site_prefix" {
+variable "github_repo" {
+  description = "The GitHub repository to link with Netlify (e.g. username/repo)"
   type        = string
-  description = "Welcome to Terraform 30-days Chanllenge"
-  default     = "hug-ibadan-demo"
+  default = "LIGHTHINGS/terraform-challenge"
+}
+
+variable "branch" {
+  description = "The branch in the GitHub repository to deploy"
+  type        = string
+  default     = "main"
+}
+
+variable "site_prefix" {
+  description = "Simple Terraform Demo Website"
+  type        = string
+  default     = "hug-demo"
 }
