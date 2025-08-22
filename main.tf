8677023efb2b3c008ba93a38f621a7e1d8c8cecb
Dare Omotayo <dareomotayo32@gmail.com>
2025-08-22 21:47:34 +0100

    change provider to AegirHealth with the lastest version

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     netlify = {
-      source  = "netlify/netlify"
-      version = "~> 0.2"
+      source  = "AegirHealth/netlify"
+      version = "0.6.12" # <-- check latest version on registry
     }
     random = {
       source  = "hashicorp/random"
@@ -11,10 +11,9 @@ terraform {
   }
 
   cloud {
-    organization = "HUG-Ibadan1"   # replace with your HCP Terraform org
-
+    organization = "HUG-Ibadan1"   # your HCP Terraform org
     workspaces {
-      name = "netlify-challenge"     # replace with your HCP Terraform workspace
+      name = "terraform-challenge" # your HCP Terraform workspace
     }
   }
 }
@@ -27,15 +26,17 @@ resource "random_pet" "site" {
   length = 2
 }
 
+# Creates a Netlify site linked to a GitHub repo
 resource "netlify_site" "demo" {
   name = "${var.site_prefix}-${random_pet.site.id}"
+
+  repo {
+    provider      = "github"
+    repo_path     = var.github_repo   # e.g. "yourusername/your-repo"
+    repo_branch   = var.branch        # e.g. "main"
+    deploy_key_id = netlify_deploy_key.key.id
+  }
 }
 
-resource "netlify_deploy" "site_files" {
-  site_id = netlify_site.demo.id
-  dir     = "${path.module}/." # deploys your index.html
-}
-
-output "live_site_url" {
-  value = netlify_site.demo.ssl_url
-}
+# Creates a deploy key for GitHub access
+resource "netlify_deploy_key" "key" {}
